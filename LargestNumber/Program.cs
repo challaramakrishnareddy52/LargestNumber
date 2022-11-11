fa480df50275ddeeb5968b691b71ac20246def4a
challaramakrishnareddy52 <challaramakrishnareddy52@gmail.com>
2022-11-11 20:43:43 +0530

    Largest Number

---
+++ LargestNumber/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace LargestNumber
+{
+    class Program
+    {
+        static void largest(int x, int y, int z)
+        {
+            int max = x;
+
+            if (x >= y && x >= z)
+                max = x;
+            if (y >= x && y >= z)
+                max = y;
+            if (z >= x && z >= y)
+                max = z;
+
+            Console.WriteLine("largest number among {0}, {1} and {2} is: {3}",x, y, z, max);
+        }
+
+        static void Main(string[] args)
+        {
+            largest(100, 50, 25);
+            largest(50, 50, 75);
+            largest(20, 50, 30);
+        }
+    }
+}
